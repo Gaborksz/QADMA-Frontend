d4a4028018a274d594dd5d60cd8d78ef6d0b6e37
Kovács Szabó Gábor <64842590+Gaborksz@users.noreply.github.com>
2025-09-16 10:50:28 +0200

    Form logic moved back into component

--- src/app/features/home/change-management/components/change-note-form/change-note-form.component.ts
+++ src/app/features/home/change-management/components/change-note-form/change-note-form.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { ChangeNoteFormService } from '../../service/change-note-form.service';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AuthService } from '../../../../auth/services/auth.service';
+import { FormMode } from '../../../../../shared/model/form.mode.enum';
+import { ChangeNote } from '../../model/change-note';
+import { BasicFormControl } from '../../../../../shared/model/basic-form-control';
+import { FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-change-note-form',
@@ -9,15 +13,42 @@ import { ChangeNoteFormService } from '../../service/change-note-form.service';
 })
 export class ChangeNoteFormComponent {
 
-
+  @Input() mode!: FormMode
+  @Input() parentForm!: FormGroup;
   changeNoteForm!: FormGroup;
 
-  constructor(private changeNoteFormService: ChangeNoteFormService) {
-    this.changeNoteForm = changeNoteFormService.changeNoteForm;
-  }
-  
+  @Output() updatedChangeNote = new EventEmitter<ChangeNote>();
 
-  getControl(contolName: string): FormControl {
-    return this.changeNoteForm.get(contolName) as FormControl;
+
+  constructor(private authService: AuthService) {
+
+    this.changeNoteForm = new FormGroup({
+      id: new BasicFormControl({ value: 0, disabled: true }),
+      changeDescription: new BasicFormControl({ value: { id: 1, nev: 'kutya' }, disabled: true }),
+      dateCreated: new BasicFormControl({ value: '', disabled: true }),
+      createdBy: new BasicFormControl({ value: '', disabled: false })
+    });
+
+    this.changeNoteForm.valueChanges.subscribe(value => {
+      const changeNote = new ChangeNote();
+      changeNote.id = value.id;
+      changeNote.changeDescription = value.changeDescription;
+      changeNote.dateCreated = value.dateCreated;
+    })
+  }
+
+
+  ngOnInit() {
+    if (this.parentForm != null) { this.parentForm.addControl('changeNote', this.changeNoteForm); }
+    if (this.mode == null) { this.mode = FormMode.Display }
+  }
+
+
+  getControl(contolName: string): BasicFormControl {
+    return this.changeNoteForm.get(contolName) as BasicFormControl;
+  }
+
+  doit() {
+    console.log("doit");
   }
 }
