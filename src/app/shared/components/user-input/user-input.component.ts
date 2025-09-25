a1b328cf8d59ab30bcef41070ef9e4d773aec4b1
Kovács Szabó Gábor <64842590+Gaborksz@users.noreply.github.com>
2025-09-25 16:10:54 +0200

    add user-input component

--- src/app/shared/components/user-input/user-input.component.ts
+++ src/app/shared/components/user-input/user-input.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { BasicFormControl } from '../../model/basic-form-control';
-import { UserService } from '../../services/user.service';
-import { QadmaUser } from '../../model/qadma-user';
+import { Validators } from '@angular/forms';
+
+import { debounceTime, distinctUntilChanged, filter, skip, switchMap, tap } from 'rxjs';
+import { BasicFormControl } from '../../../core/model/basic-form-control';
+import { QadmaUser } from '../../../core/model/qadma-user';
+import { UserService } from '../../../core/services/user.service';
 
 @Component({
   selector: 'app-user-input',
@@ -13,85 +15,109 @@ export class UserInputComponent {
 
   @Input() label = '';
   @Input() inputControl!: BasicFormControl;
-  nameControl!: FormControl;
-  idControl!: FormControl;
+  nameControl!: BasicFormControl;
+  idControl!: BasicFormControl;
   users: QadmaUser[] = [];
+  noMatchResults = false;
 
 
   constructor(private userService: UserService) { }
 
 
   ngOnInit() {
-    this.nameControl = new FormControl('');
-    this.nameControl.setValue(this.inputControl.value?.username);
+    this.nameControl = new BasicFormControl('');
+    this.idControl = new BasicFormControl('', [Validators.min(1)]);
 
-    this.idControl = new FormControl('', [Validators.min(1)]);
-    this.idControl.setValue(this.inputControl.value?.id);
+    this.nameControl.setValue(this.inputControl.value.username, { emitEvent: false })
+    this.idControl.setValue(this.inputControl.value.id, { emitEvent: false })
 
-    this.inputControl.valueChanges.subscribe(user => {
+    this.inputControl.valueChanges.subscribe(value => {
 
-      if (user?.id > 0) {
-        this.idControl.setValue(user?.id);
+      const user = new QadmaUser();
+      user.id = value?.id;
+      user.username = value?.username;
+      user.authorities = value?.authorities === null ? [] : value.authorities;
+
+      const previousUser = new QadmaUser();
+      previousUser.id = this.inputControl.previousValue?.id;
+      previousUser.username = this.inputControl.previousValue?.userName;
+      previousUser.authorities = this.inputControl.previousValue?.authorities === null ? [] : value.authorities;
+
+      if (!user.equalTo(previousUser)) {
+        this.idControl.setValue(user?.id > 0 ? user.id : '');
         this.nameControl.setValue(user.username);
-      }
-
-      if (user?.id === 0 && (this.idControl.value !== '' || this.nameControl.value !== '')) {
-        this.idControl.setValue('');
-        this.nameControl.setValue('');
       }
     })
 
+    this.nameControl.valueChanges
+      .pipe(
+        skip(2),
+        debounceTime(300),
+        distinctUntilChanged(),
+        tap((value: string) => {
+          if (value === '') {
+            this.users = [];
+            this.setUser(new QadmaUser());
+            this.noMatchResults = false;
+          }
+        }),
+        filter((value: string) => value?.length > 0),
+        switchMap((searchTerm) => this.userService.findUsersBySearchTerm(searchTerm)),
+        tap(users => {
+          if (users.length === 0) { this.users = [] }
+          if (users.length === 1 && users[0].username === this.nameControl.value) {
+            this.inputControl.setValue(users[0]);
+          }
+        }),
+        filter((users) => !(users.length === 1 && users[0].username === this.nameControl.value))
+      ).subscribe(users => {
+        this.users = users;
+        this.users.length === 0 ? this.noMatchResults = true : this.noMatchResults;
+      })
+
+
+    this.idControl.valueChanges
+      .pipe(
+        skip(2),
+        debounceTime(700),
+        distinctUntilChanged(),
+        tap(id => { if (id == '') { this.setUser(new QadmaUser()); } }),
+        filter(id => id > 0),
+        switchMap(id => this.userService.findUserById(id))
+      )
+      .subscribe(user => {
+        if (user?.id > 0) {
+          this.setUser(user);
+        } else {
+          this.setUser(new QadmaUser());
+          this.noMatchResults = true
+        }
+      })
+
+
     this.inputControl.enabledStatus.subscribe(enabledStatus => {
+      this.nameControl.disable();
+      this.idControl.disable()
+
       if (enabledStatus) {
         this.nameControl.enable();
         this.idControl.enable()
-      } else {
-        this.nameControl.disable();
-        this.idControl.disable()
       }
     })
   }
 
-
-  findUsersBySearchTerm(searchTerm: string) {
-
-    if (searchTerm.length === 0) {
-      this.inputControl.setValue(new QadmaUser());
-      this.users = [];
-      return;
-    }
-
-    if (searchTerm != this.inputControl.value.username) {
-      this.userService.findUsersBySearchTerm(searchTerm).subscribe(users => {
-        this.users = users;
-      })
-    }
-  }
-
-  findUsersById(userId: string) {
-
-    const id = Number.parseInt(userId);
-
-    if (this.idControl.valid) {
-      this.userService.findUserById(id).subscribe(user => {
-        this.setUser(user);
-      })
-    }
-  }
-
-
   setUser(user: QadmaUser) {
-    this.inputControl.setValue(user);
+    this.inputControl.setValue(user)
   }
-
 
   onBlur() {
     this.users = [];
-
-    if (this.inputControl.value.id > 0) {
-      this.inputControl.updateValueAndValidity({ emitEvent: true });
-    } else {
+    if (this.noMatchResults) {
       this.inputControl.setValue(new QadmaUser());
+    } else {
+      this.inputControl.updateValueAndValidity();
     }
+
+    this.noMatchResults = false;
   }
 }
