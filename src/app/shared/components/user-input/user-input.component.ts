f436baa2471362df1ba9cdea1b0823947b0a858e
Kovács Szabó Gábor <64842590+Gaborksz@users.noreply.github.com>
2025-10-24 21:48:55 +0200

    optimised variable initialization and logic

--- src/app/shared/components/user-input/user-input.component.ts
+++ src/app/shared/components/user-input/user-input.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Validators } from '@angular/forms';
 
-import { debounceTime, distinctUntilChanged, filter, skip, switchMap, tap } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, filter, of, startWith, switchMap, tap } from 'rxjs';
 import { BasicFormControl } from '../../../core/model/basic-form-control';
 import { QadmaUser } from '../../../core/model/qadma-user';
 import { UserService } from '../../../core/services/user.service';
@@ -15,84 +15,86 @@ export class UserInputComponent {
 
   @Input() label = '';
   @Input() inputControl!: BasicFormControl;
-  nameControl!: BasicFormControl;
-  idControl!: BasicFormControl;
+  nameControl = new BasicFormControl('');
+  idControl = new BasicFormControl('', [Validators.min(1)]);
   users: QadmaUser[] = [];
   noMatchResults = false;
 
 
-  constructor(private userService: UserService) { }
-
-
-  ngOnInit() {
-    this.nameControl = new BasicFormControl('');
-    this.idControl = new BasicFormControl('', [Validators.min(1)]);
-
-    this.nameControl.setValue(this.inputControl.value.username, { emitEvent: false })
-    this.idControl.setValue(this.inputControl.value.id, { emitEvent: false })
-
-    this.inputControl.valueChanges.subscribe(value => {
-
-      const user = new QadmaUser();
-      user.id = value?.id;
-      user.username = value?.username;
-      user.authorities = value?.authorities === null ? [] : value.authorities;
-
-      const previousUser = new QadmaUser();
-      previousUser.id = this.inputControl.previousValue?.id;
-      previousUser.username = this.inputControl.previousValue?.userName;
-      previousUser.authorities = this.inputControl.previousValue?.authorities === null ? [] : value.authorities;
-
-      if (!user.equalTo(previousUser)) {
-        this.idControl.setValue(user?.id > 0 ? user.id : '');
-        this.nameControl.setValue(user.username);
-      }
-    })
+  constructor(private userService: UserService) {
 
     this.nameControl.valueChanges
       .pipe(
-        skip(2),
         debounceTime(300),
         distinctUntilChanged(),
         tap((value: string) => {
-          if (value === '') {
+          this.noMatchResults = false;
+
+          if (value.trim() === '') {
             this.users = [];
-            this.setUser(new QadmaUser());
-            this.noMatchResults = false;
+            if (this.inputControl.value?.id > 0) { this.setUser(new QadmaUser()) };
           }
         }),
-        filter((value: string) => value?.length > 0),
-        switchMap((searchTerm) => this.userService.findUsersBySearchTerm(searchTerm)),
-        tap(users => {
-          if (users.length === 0) { this.users = [] }
-          if (users.length === 1 && users[0].username === this.nameControl.value) {
-            this.inputControl.setValue(users[0]);
-          }
-        }),
-        filter((users) => !(users.length === 1 && users[0].username === this.nameControl.value))
+        filter(value => value !== this.inputControl.value?.username),
+        filter(value => value?.trim().length > 0),
+        switchMap((searchTerm) => this.userService.findUsersBySearchTerm(searchTerm))
       ).subscribe(users => {
         this.users = users;
-        this.users.length === 0 ? this.noMatchResults = true : this.noMatchResults;
-      })
+        this.users.length === 0 ? this.noMatchResults = true : this.noMatchResults = false;
 
-
-    this.idControl.valueChanges
-      .pipe(
-        skip(2),
-        debounceTime(700),
-        distinctUntilChanged(),
-        tap(id => { if (id == '') { this.setUser(new QadmaUser()); } }),
-        filter(id => id > 0),
-        switchMap(id => this.userService.findUserById(id))
-      )
-      .subscribe(user => {
-        if (user?.id > 0) {
-          this.setUser(user);
-        } else {
-          this.setUser(new QadmaUser());
-          this.noMatchResults = true
+        if (users.length === 1 && users[0].username === this.nameControl.value) {
+          this.inputControl.setValue(users[0]);
+          this.users = [];
         }
       })
+
+
+    this.idControl.valueChanges.pipe(
+      debounceTime(700),
+      distinctUntilChanged(),
+      tap(id => {
+        if (id === '' || id === null) {
+          this.setUser(new QadmaUser());
+          this.noMatchResults = false;
+        }
+      }),
+      filter(id => id > 0),
+      switchMap(id =>
+        this.userService.findUserById(id)
+          .pipe(
+            catchError(() => of(null))
+          )
+      )
+    ).subscribe(user => {
+      if (user) {
+        this.setUser(user);
+        this.noMatchResults = false;
+      } else {
+        this.noMatchResults = true;
+      }
+    });
+  }
+
+
+  ngOnInit() {
+
+    this.inputControl.valueChanges
+      .pipe(startWith(this.inputControl.value))
+      .subscribe(value => {
+
+        const { id, username, authorities } = value
+        const user = new QadmaUser(id, username, authorities);
+
+        const previousUser = new QadmaUser(this.inputControl.previousValue);
+
+        if (!user.equalTo(previousUser)) {
+
+          this.idControl.setValue(user?.id > 0 ? user.id : '');
+
+          this.nameControl.setValue(user.username);
+        }
+      })
+
 
 
     this.inputControl.enabledStatus.subscribe(enabledStatus => {
@@ -106,18 +108,20 @@ export class UserInputComponent {
     })
   }
 
+
   setUser(user: QadmaUser) {
     this.inputControl.setValue(user)
   }
 
+
   onBlur() {
-    this.users = [];
     if (this.noMatchResults) {
       this.inputControl.setValue(new QadmaUser());
     } else {
       this.inputControl.updateValueAndValidity();
     }
 
+    this.users = [];
     this.noMatchResults = false;
   }
 }
