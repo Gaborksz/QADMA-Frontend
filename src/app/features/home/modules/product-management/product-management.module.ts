71c8b29d8940b7df843d3310c3ec9d09f1b7f3af
Kovács Szabó Gábor <64842590+Gaborksz@users.noreply.github.com>
2025-10-24 21:45:11 +0200

    add ProductCreateHomeComponent import

--- src/app/features/home/modules/product-management/product-management.module.ts
+++ src/app/features/home/modules/product-management/product-management.module.ts
@@ -1,38 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProductManagementRoutingModule } from './product-management-routing.module';
 import { ProductManagementHomeComponent } from './components/product-management-home/product-management-home.component';
-import { ProductSearchComponent } from './components/product-search/product-search.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ProductSearchResultComponent } from './components/product-search-result/product-search-result.component';
-import { NoSearchResultComponent } from './components/no-search-result/no-search-result.component';
 import { ProductCreateHomeComponent } from './components/product-create-home/product-create-home.component';
-import { ProductFormComponent } from './components/product-form/product-form.component';
 import { InspectionManagementModule } from '../inspection-management/inspection-management.module';
 import { SharedModule } from '../../../../shared/shared.module';
-
-
-
+import { SharedFeaturesModule } from '../../../../shared-features/shared-features.module';
 
 @NgModule({
   declarations: [
     ProductManagementHomeComponent,
-    ProductSearchComponent,
-    ProductSearchResultComponent,
-    NoSearchResultComponent,
-    ProductCreateHomeComponent,
-    ProductFormComponent
+    ProductCreateHomeComponent
   ],
   imports: [
     CommonModule,
     ProductManagementRoutingModule,
     ReactiveFormsModule,
     InspectionManagementModule,
+    SharedFeaturesModule,
     SharedModule
   ],
-  exports: [
-    ProductFormComponent
-  ]
+  exports: []
 })
 export class ProductManagementModule { }
