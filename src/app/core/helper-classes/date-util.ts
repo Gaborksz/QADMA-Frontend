4bafbd583da1a93b0e62ee24bc8d2450ef3b19a4
Kovács Szabó Gábor <64842590+Gaborksz@users.noreply.github.com>
2025-10-24 21:08:53 +0200

    add null handling

--- src/app/core/helper-classes/date-util.ts
+++ src/app/core/helper-classes/date-util.ts
@@ -1,7 +1,12 @@
 
 export class DateUtil {
 
-    static dateAsString(date: Date): string {
-        return date.toISOString().substring(0, 10);
+    static dateAsString(date: Date | null): string {
+
+        if (date && date instanceof Date) {
+            return date.toISOString().substring(0, 10);
+        }
+
+        return '';
     }
 }
