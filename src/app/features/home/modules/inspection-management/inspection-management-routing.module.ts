30914a240ca7221a548368fd5776921093853835
Kovács Szabó Gábor <64842590+Gaborksz@users.noreply.github.com>
2025-10-24 21:38:18 +0200

    add Inspection Template Search and Create routes

--- src/app/features/home/modules/inspection-management/inspection-management-routing.module.ts
+++ src/app/features/home/modules/inspection-management/inspection-management-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InspectionManagementHomeComponent } from './components/inspection-management-home/inspection-management-home.component';
+import { InspectionTemplateSearchComponent } from '../../../../shared-features/components/inspection-management/inspection-template-search/inspection-template-search.component';
+import { InspectionTemplateChangeNoteCreateComponent } from '../../../../shared-features/components/change-management/inspection-template-change-note-create/inspection-template-change-note-create.component';
+
 
 const routes: Routes = [
   {
-    path:'', component: InspectionManagementHomeComponent 
+    path: '', component: InspectionManagementHomeComponent,
+    children: [
+      { path: 'search', component: InspectionTemplateSearchComponent },
+      { path: 'create', component: InspectionTemplateChangeNoteCreateComponent }
+    ]
   }
 ];
 
